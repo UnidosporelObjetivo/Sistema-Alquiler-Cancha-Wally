1a8bf9fe541410ac27319f29413f4a7a728185df
Wilfredo <villcaf175@gmail.com>
2024-06-10 19:24:49 -0400

    CRUD LOGUIN

---
+++ Sistema Alquiler Cancha  Wally/Controllers/LoguinController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema_Alquiler_Cancha__Wally.Controllers
+{
+    public class LoguinController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
