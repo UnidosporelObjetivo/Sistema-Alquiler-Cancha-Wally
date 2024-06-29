ed1bb5252ce8c8752608efc10f985f08971dd13d
Wilfredo <127908862+villcaf175@users.noreply.github.com>
2024-06-29 10:48:56 -0400

    texto

---
+++ Sistema Alquiler Cancha  Wally/Controllers/LoguinController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema_Alquiler_Cancha__Wally.Context;
+using Sistema_Alquiler_Cancha__Wally.Models;
+
+namespace Sistema_Alquiler_Cancha__Wally.Controllers
+{
+    public class LoguinController : Controller
+    {
+        private MyContext _Context;
+        public LoguinController(MyContext context)
+        {
+            _Context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //crud login
+        [HttpPost]
+        public async Task<IActionResult> Login(String email, String password)
+        {
+            var usuario = await _Context.Usuarios
+                           .Where(x => x.Email == email)
+                           .Where(x=>x.Password == password)
+                          .FirstOrDefaultAsync();
+            if (usuario != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                //mandando mensajes a la vista
+                TempData["LoginError"] = "Cuenta o Password incorrectos!";
+                return RedirectToAction("Index", "Login");
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
